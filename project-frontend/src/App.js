d8c504509ab2a12c886fb476716c45218b459d9c
Jonathan McCaffrey <jmccaffrey42@gmail.com>
2018-11-26 00:05:11 -0800

    Added basic routes and login screen

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,28 +1,118 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Router, Route, Link, Redirect} from "react-router-dom";
 import './App.css';
+import history from './history';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class LoginScreen extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            error: null
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        fakeAuth.authenticate(() => {
+            history.push('/');
+        });
+    }
+
+    rendeError() {
+        if (!this.state.error) {
+            return;
+        }
+        return (
+            <div className="error">
+                {this.state.error}
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="loginScreen">
+                <div className="dialog dialogSmall centered">
+                    <header>
+                        <h1>Login</h1>
+                        or <Link to="/register">create an account</Link>
+                    </header>
+                    <form onSubmit={this.handleSubmit.bind(this)}>
+
+                        {this.rendeError()}
+
+                        <div className="inputGroup">
+                            <div className="formInput">
+                                <label>Email Address:</label>
+                                <input type="text" placeholder="email address"/>
+                            </div>
+                            <div className="formInput">
+                                <label>Password</label>
+                                <input type="text" placeholder="no password required" disabled="disabled" />
+                            </div>
+                        </div>
+
+
+                        <footer>
+                            <div className="formButtons">
+                                <button type="submit" className="button buttonFullWidth buttonGreen">Login</button>
+                            </div>
+                        </footer>
+                    </form>
+
+                </div>
+            </div>
+        );
+    }
 }
+
+const IndexScreen = () => <h2>Home</h2>;
+const RegisterScreen = () => <h2>Register</h2>;
+const BoardScreen = () => <h2>Board</h2>;
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true
+        setTimeout(cb, 100)
+    },
+    signout(cb) {
+        this.isAuthenticated = false
+        setTimeout(cb, 100)
+    }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        fakeAuth.isAuthenticated === true
+            ? <Component {...props} />
+            : <Redirect to='/login' />
+    )} />
+);
+
+const App = () => (
+    <Router history={history}>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/board">Board</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Route path="/" exact component={IndexScreen} />
+            <Route path="/login/" component={LoginScreen} />
+            <Route path="/register/" component={RegisterScreen} />
+            <PrivateRoute path="/board/" component={BoardScreen} />
+        </div>
+    </Router>
+);
 
 export default App;
